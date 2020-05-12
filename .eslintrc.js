418c44e4c0bfc4cd137ea5851b4c248c84058c9d
Mr.CactusX <1106511295@qq.com>
2020-05-12 11:03:25 +0800

    master

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Mr.Cactus
+ * @Date: 2020-05-11 16:04:14
+ * @LastEditors: Mr.Cactus
+ * @LastEditTime: 2020-05-12 10:58:47
+ */
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/essential',
+
+	],
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+	}
 }
---
+++ src/components/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    ok
+  </div>
+</template>
+<style scoped>
+@import url('../assets/login.css');
+</style>
+<script>
+export default {
+  name: 'login'
+}
+</script>
