3b761293bf339b9b00d4f244c519f57e84ddd621
Mr.CactusX <1106511295@qq.com>
2020-05-14 00:00:20 +0800

    user

---
+++ src/components/subComponents/breadcrumb.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: Mr.Cactus
+ * @Date: 2020-05-13 21:13:46
+ * @LastEditors: Mr.Cactus
+ * @LastEditTime: 2020-05-13 21:31:52
+ -->
+<template>
+  <el-breadcrumb separator-class="el-icon-arrow-right">
+    <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>{{firstmenu}}</el-breadcrumb-item>
+    <el-breadcrumb-item>{{secmenu}}</el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+<style scoped>
+</style>
+<script>
+export default {
+  name: 'breadcrumb',
+  props: [
+      'firstmenu',
+      'secmenu'
+  ]
+}
+</script>
